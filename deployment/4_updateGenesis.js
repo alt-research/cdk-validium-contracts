5b132a0ebb43b8f87aa762fb90ab0ae66968c343
Jiankun Yang <110372666+0xyjk@users.noreply.github.com>
2024-01-29 15:29:12 +0800

    feat: add dockerfile and support update genesis

---
+++ deployment/4_updateGenesis.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const deployOutput = require('./deploy_output.json');
+const genesis = require('./genesis.json');
+
+async function main() {
+    console.log('\n#######################');
+    console.log('#####  Update Genesis  #####');
+    console.log('#######################');
+
+    l1ChainId = Number(process.env.L1_CHAIN_ID)
+    if (!Number.isInteger(l1ChainId)) {
+        throw new Error('env L1_CHAIN_ID is not an integer');
+    }
+    const deploymentBlockNumber = deployOutput.deploymentBlockNumber;
+    genesis.genesisBlockNumber = deploymentBlockNumber;
+    console.log('genesisBlockNumber set to:', deploymentBlockNumber);
+
+    genesis.l1Config = {
+        chainId: l1ChainId,
+        cdkValidiumAddress: deployOutput.cdkValidiumAddress,
+        maticTokenAddress: deployOutput.maticTokenAddress,
+        polygonZkEVMGlobalExitRootAddress: deployOutput.polygonZkEVMGlobalExitRootAddress,
+        cdkDataCommitteeContract: deployOutput.cdkDataCommitteeContract
+    }
+    console.log('l1Config set to:', genesis.l1Config);
+    fs.writeFileSync(path.join(__dirname, 'genesis.json'), JSON.stringify(genesis, null, 4));
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
